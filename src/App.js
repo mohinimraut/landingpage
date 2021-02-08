5929835f2de3cc90c1aa12035b04504812a5ddc3
mohinimraut <mohiniraut7585@gmail.com>
2021-02-08 16:00:34 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Launch from './Launch'
+function App() {
+  return (
+    <div className="App">
+      <Launch/>
+    </div>
+  );
+}
+
+export default App;
